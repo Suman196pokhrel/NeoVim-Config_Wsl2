35043362ae7ff6af16cccf9d01de3359209e3337
Suman Pokhrel <spokhrel196@gmail.com>
2023-09-05 18:12:29 +0545

    initialized git repo

---
+++ lua/core/keymaps.lua
@@ -0,0 +1,16 @@
+vim.g.mapleader=' '
+vim.g.maplocalleader=' '
+
+vim.opt.backspace='2'
+vim.opt.showcmd=true
+vim.opt.laststatus=2
+vim.opt.autowrite=true
+vim.opt.cursorline=false
+vim.opt.autoread=true
+
+
+-- Use spaces for tabs and whatnot
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab=true
+vim.opt.shiftround=true
---
+++ lua/core/plugin_config/tokyonight.lua
@@ -0,0 +1,8 @@
+-- tokyonight.lua
+vim.g.tokyonight_style = "night" -- There are three styles: storm, night, and day.
+
+
+
+-- Set the colorscheme
+vim.cmd[[colorscheme tokyonight]]
+
